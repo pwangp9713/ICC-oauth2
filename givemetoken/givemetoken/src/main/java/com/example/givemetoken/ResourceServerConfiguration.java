5fe6fff9a70fddf3f58fca7f7b8fd2b81925b9e3
pwangp9713@hotmail.com <>
2019-02-22 18:27:28 +0800

    jdbcClientDetails+jwtTokenStore

--- givemetoken/givemetoken/src/main/java/com/example/givemetoken/ResourceServerConfiguration.java
+++ givemetoken/givemetoken/src/main/java/com/example/givemetoken/ResourceServerConfiguration.java
@@ -5,6 +5,8 @@
  */
 package com.example.givemetoken;
 
+import java.util.HashMap;
+import java.util.Map;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -12,15 +14,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 /**
  *
@@ -41,9 +49,38 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 //        remoteTokenServices.setClientSecret("abcd");
 
         DefaultTokenServices dts = new DefaultTokenServices();
-        dts.setTokenStore(new JdbcTokenStore(dataSource));
+        //dts.setTokenStore(new JdbcTokenStore(dataSource));
+        dts.setTokenEnhancer(accessTokenConverter());
+        dts.setTokenStore(new JwtTokenStore(accessTokenConverter()));
         resources.tokenServices(dts);
 
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+
+        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter() {
+            @Override
+            public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+
+                String userName = authentication.getUserAuthentication().getName();
+                // 与登录时候放进去的UserDetail实现类一直查看link{SecurityConfiguration}
+                User user = (User) authentication.getUserAuthentication().getPrincipal();
+                /**
+                 * 自定义一些token属性 **
+                 */
+                final Map<String, Object> additionalInformation = new HashMap<>();
+                additionalInformation.put("userName", userName);
+                additionalInformation.put("roles", user.getAuthorities());
+                ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInformation);
+                OAuth2AccessToken enhancedToken = super.enhance(accessToken, authentication);
+                return enhancedToken;
+            }
+
+        };
+
+        accessTokenConverter.setSigningKey("123");
+        return accessTokenConverter;
     }
 
     @Override
